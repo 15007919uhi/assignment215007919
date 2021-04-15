45c674f1e77c1a536d4cdeaa72e34a01abbc30ed
Kirsten Rosie <15007919@uhi.ac.uk>
2021-04-15 12:33:19 +0100

    Created basic components and set up Firebase tools

---
+++ src/components/QuestionSection.js
@@ -0,0 +1,15 @@
+function QuestionSection () {
+    return (
+        <div className="col-sm">
+            <h3 className="text-center">Question</h3>
+            <div className="p-3 mb-2 bg-light">
+                <div className="text-center">
+                    <img className="mb-4 rounded img-fluid" src="https://i-want-to-study-engineering.org/figs/balances.png" alt="" />
+                </div>
+                <p>Three balances are supported on rigid pivots and connected by two flexible light strings as shown in the diagram above.</p>
+            </div>
+        </div>
+    );
+}
+
+export default QuestionSection;
---
+++ src/pages/QuestionPage.js
@@ -0,0 +1,20 @@
+import AnswerSection from "../components/AnswerSection";
+import QuestionSection from "../components/QuestionSection";
+import HintsSection from "../components/HintsSection";
+
+function QuestionPage() {
+    return (
+        <div className="container">
+            <h1 className="text-center">Question Page</h1>
+            <div className="row">
+                <QuestionSection />
+                <HintsSection />
+            </div>
+            <div className="row">
+                <AnswerSection />
+            </div>
+        </div>
+    );
+}
+
+export default QuestionPage;
