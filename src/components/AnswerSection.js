f6e9e88a5801e770c32e386bc0ce982338d5b934
15007919uhi <15007919@uhi.ac.uk>
2021-05-11 10:45:57 +0100

    Answers now accessed through Firestore

--- src/components/AnswerSection.js
+++ src/components/AnswerSection.js
@@ -1,16 +1,37 @@
+import React, { useEffect, useState } from 'react'
+import { firestore } from '../services/firebase'
+
 function AnswerSection() {
+    const [title1, setTitle1] = useState("")
+    const [title2, setTitle2] = useState("")
+    const [title3, setTitle3] = useState("")
+    const [title4, setTitle4] = useState("")
+    const [answerText, setanswerText] = useState("")
+    const [imageUri, setImageUri] = useState("")
+    const answerId = "balances" //Should probably be done through URL and routing with useParams()
+
+    useEffect(async () => {
+        const snapshot = await firestore.collection("Answers").doc(answerId).get()
+        const answerData = snapshot.data()
+        const answerDetails = answerData[answerId]
+        setTitle1(answerDetails[answerId].answer.ans1.title)
+        setTitle2(answerDetails[answerId].answer.ans2.title)
+        setTitle3(answerDetails[answerId].answer.ans3.title)
+        setTitle4(answerDetails[answerId].answer.ans4.title)
+    },[])
+
     return (
         <div className="col-12">
         <h3 className="text-center">Answers</h3>
         <div className="p-3 mb-2 bg-light">
             <div className="row">
                 <div className="col-sm d-grid gap-2">
-                    <button type="button" className="btn btn-secondary mb-2 p-4">10 kg</button>
-                    <button type="button" className="btn btn-secondary mb-2 p-4">1260 kg</button>
+                    <button type="button" className="btn btn-secondary mb-2 p-4">{title1}</button>
+                    <button type="button" className="btn btn-secondary mb-2 p-4">{title2}</button>
                 </div>
                 <div className="col-sm d-grid gap-2">
-                    <button type="button" className="btn btn-secondary mb-2 p-4">1 17/28 kg</button>
-                    <button type="button" className="btn btn-secondary mb-2 p-4">62 2/9 kg</button>
+                    <button type="button" className="btn btn-secondary mb-2 p-4">{title3}</button>
+                    <button type="button" className="btn btn-secondary mb-2 p-4">{title4}</button>
                 </div>
             </div>
             <div className="row">
